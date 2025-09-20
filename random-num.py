681b2426e7a0ea9bb12fd9decab15eca83776781
Taliyah Agbesi <81756449+tillylori@users.noreply.github.com>
2025-09-20 15:04:42 +0000

    Create random-num.py

---
+++ random-num.py
@@ -0,0 +1,6 @@
+#I have imported the random library so I can use the randomint() function
+import random 
+
+#I'm using randint with the range of 1,000,000
+randomnum= random.randint(0,1000000)
+print("Your Random Number is:" + str(randomnum))
